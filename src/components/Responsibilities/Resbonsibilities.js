c70b7b44aa77334784a179426d8296aef7d07cf1
Ogundipe Shamsondeen <younglex62@gmail.com>
2020-05-12 04:28:14 +0100

    code review

--- src/components/Responsibilities/Resbonsibilities.js
+++ src/components/Responsibilities/Resbonsibilities.js
@@ -1,8 +1,5 @@
 import React, { Component } from 'react';
-import academic from '../../assets/01.png';
-import Hostel from '../../assets/03.png';
-import Welfare from '../../assets/04.png';
-import Transport from '../../assets/06.png';
+
 
 import './Responsibilities.css'
 
@@ -11,65 +8,7 @@ export default class Responbilities extends Component {
         return (
             <React.Fragment>
                   {/*Start Of Responsibilties*/}
-                  <div className="responsibilities">
-                        <div className="container">
-                            <div className="row mb-3">
-                                <div className="col-lg-3 col-sm-6 ">
-                                    <div className="responsibility">
-                                        <img src={academic} className="responsibility__icon " />
-                                        <div>
-                                            <h3>Academic and Career Talk</h3>
-                                            <p>
-                                            Organises career talks, inspirational and motivational lectures among the students.
-                                        </p>
-                                        </div>
-
-                                    </div>
-                                </div>
-                                <div className="col-lg-3 col-sm-6 ">
-                                    <div className="responsibility">
-                                        <img src={Hostel} className="responsibility__icon" />
-                                        <div>
-                                            <h3 className="heading-tertiary">Accommodation</h3>
-                                            <p>
-                                            Supervises the use of facilities and renovations in the hostels on campus during the holiday.
-                                        </p>
-                                        </div>
-
-                                    </div>
-                                </div>
-                                <div className="col-lg-3 col-sm-6">
-                                    <div className="responsibility">
-                                        <img src={Transport} className="responsibility__icon" />
-                                        <div>
-                                            <h3 className="heading-tertiary">Transportation</h3>
-                                            <p>
-                                            Through its Transport Scheme, shuttle buses are provided for transportation at a subsidised rate.
-                                        </p>
-                                        </div>
-
-                                    </div>
-                                </div>
-                                <div className="col-lg-3 col-sm-6 ">
-                                    <div className="responsibility">
-                                        <img src={Welfare} className="responsibility__icon" />
-                                        <div>
-                                            <h3 className="heading-tertiary">Welfarism</h3>
-                                            <p>
-                                            Handles all matters relating to the general welfare of members of the Union and all the Union guests.
-                                        </p>
-                                        </div>
-
-                                    </div>
-                                </div>
-                            </div>
-
-                        </div>
-                    </div>
-
-
-
-                    {/*End Of Responsibilties*/}
+                  
             </React.Fragment>
         )
     }
